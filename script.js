aa9a5c77fcde99d51c3755f3c7c43d4640a6b863
ALeo75C <za.iz.pr@yandex.kz>
2019-04-09 13:21:40 +0300

    конецПары

--- script.js
+++ script.js
@@ -35,7 +35,7 @@ function loadHistory() {
   const historySize = localStorage.length
 
   if (historySize > 0) {
-    for (let i = 0; i < 5; i++) {
+    for (let i = 0; i < historySize; i++) {
       const key = localStorage.key(i)
       const taskName = localStorage.getItem(key)
 
